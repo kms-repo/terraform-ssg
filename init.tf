2d7be098fe1004a3f871827a89e3e20050bafdda
root <root@ip-172-31-31-129.ap-northeast-2.compute.internal>
2020-12-14 10:47:59 +0000

    first commit

---
+++ init.tf
@@ -0,0 +1,24 @@
+provider "aws"{
+	region = "ap-northeast-2"
+}
+
+# S3 bucket for backend
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "tf101-ssg-kimminsoo-area2-tfstate"
+
+  versioning {
+    enabled = true # Prevent from deleting tfstate file
+  }
+}
+
+# DynamoDB for terraform state lock
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-ssg-lock"
+  hash_key       = "LockID"
+  billing_mode   = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
